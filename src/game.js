e3b2151881e5c41184717db69f3f6347c30020c2
Quintero, Edmund <edmund.quintero@sungard.com>
2016-01-15 16:47:02 -0500

    Modular healthbar

--- src/game.js
+++ src/game.js
@@ -2,37 +2,16 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 var character;
-var graphics;
-var healthBar;
 
 function preload() {
+  game.stage.backgroundColor = '#2d2d2d';
+  character = new Player();
 }
 
 function create() {
-
-  game.stage.backgroundColor = '#2d2d2d';
-
-  graphics = game.add.graphics(0,0);
-  graphics.beginFill(0xE0042D);
-  graphics.lineStyle(2, 0xFFFFFF, 0.8);
-  healthBar = graphics.drawRect(10, 590, 780, 6);
-  graphics.endFill();
-
-  character = new Player();
-
-  character.healthTimer.loop({increment:1000, action:updateCounter});
   character.healthTimer.start();
-
 }
 
 function update() {
-  healthBar.width = Math.floor((character.health / character.maxHealth) * 780);
-}
-
-function updateCounter(){
-  character.health = character.health - 10;
-  console.log(character.health, (character.health / character.maxHealth), healthBar.width);
-  if(character.health <= 0){
-    character.healthTimer.stop();
-  }
+  character.healthTimer.update();
 }
--- src/player/healthTimer.js
+++ src/player/healthTimer.js
@@ -1,21 +1,26 @@
 "use strict";
 
 class HealthTimer {
-  constructor(){
+
+  constructor(player){
     this.game = game; // This is gathering the parent game object and relying on JS traversing.
                      // I could pass the game object to the constructor.
+
     this.timer = this.game.time.create(false);
+    this.player = player || {};
+    this.healthLossIncrement = 1;
+    this.increment = 100;
+    this.render();
   }
 
-  /*
-    Setup iteration increment and action per iteration while loop is running.
-  */
-  loop(opts){
-    opts = (typeof opts == 'object') ? opts : {};
-    this.increment = opts.increment || 1000;
-    this.action = opts.action || function(){};
-    this.timer.loop(this.increment, this.action, this);
-    return this;
+  render(){
+    let graphics = game.add.graphics(0,0);
+    graphics.beginFill(0xE0042D);
+    graphics.lineStyle(2, 0xFFFFFF, 0.8);
+    this.healthBar = graphics.drawRect(30, 590, 740, 6);
+    graphics.endFill();
+
+    this.timer.loop(this.increment, this.updateCounter, this);
   }
 
   /*
@@ -31,6 +36,27 @@ class HealthTimer {
   */
   stop(){
     this.timer.stop();
+  }
+
+  /*
+    Sets new health reduction increment.
+  */
+  setIncrement(newIncrement){
+    this.healthLossIncrement = newIncrement;
+  }
+
+  /*
+    Update function for loop.
+  */
+  updateCounter(){
+    this.player.health = this.player.health - this.healthLossIncrement;
+    if(this.player.health <= 0){
+      this.stop();
+    }
+  }
+
+  update(){
+    this.healthBar.width = Math.floor((this.player.health / this.player.maxHealth) * 780);
   }
 
 }
