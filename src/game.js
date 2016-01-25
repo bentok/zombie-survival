8fe4bbf96e159917d7fde4165969423a6ae409df
Edmund Quintero <edmundquintero@gmail.com>
2016-01-25 15:48:18 -0500

    Zombie added

--- src/game.js
+++ src/game.js
@@ -2,10 +2,12 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 var character;
+var enemy;
 
 function preload() {
   game.stage.backgroundColor = '#2d2d2d';
   character = new Player();
+  enemy = new Zombie();
 }
 
 function create() {
@@ -16,4 +18,12 @@ function create() {
 }
 
 function update() {
+
+  // enemy.sprite.body.velocity.setTo(0, 0);
+
+  // enemy.sprite.position.x -= 2;
+
+
+
+
 }
--- src/zombie/zombie.js
+++ src/zombie/zombie.js
@@ -5,6 +5,40 @@ class Zombie {
   constructor(){
     this.game = game;
     this.health = 25;
+    this.speed =3;
+
+    game.physics.enable(this, Phaser.Physics.ARCADE);
+
+    this.render();
+
+    // this.moveLeft(200);
+
   }
+
+  render(){
+
+    /*  Place holder for Zombie graphic*/
+    {
+      let zombie = this.game.add.bitmapData(25, 100);
+      zombie.ctx.fillStyle = '#FFF';
+      zombie.ctx.beginPath();
+      zombie.ctx.rect(0, 0, 25, 100);
+      zombie.ctx.fill();
+
+      this.sprite = this.game.add.sprite(this.game.world.width * 0.5 + 50, this.game.world.height - 150, zombie);
+    }
+
+  }
+
+  moveLeft(distance){
+    distance = distance || 5;
+    this.motion = game.add.tween(this.sprite).to( { x: this.sprite.position.x - distance }, distance * 100/this.speed, null, true);
+  }
+
+  moveRight(distance){
+    distance = distance || 5;
+    this.motion = game.add.tween(this.sprite).to( { x: this.sprite.position.x + distance }, distance * 100/this.speed, null, true);
+  }
+
 
 }
