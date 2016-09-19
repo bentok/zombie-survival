3b1d5e74edc60983bf9dbb49a48cef78b0de34dd
Edmund Quintero <edmundquintero@gmail.com>
2016-09-19 15:21:32 -0400

    Updates Documentation

--- src/sprites/sprites.js
+++ src/sprites/sprites.js
@@ -8,7 +8,7 @@ export class Sprites {
   load () {
     // Load spritesheet - arguments(name, image path, width, height, number of frames in image)
     this.game.load.spritesheet('player', './dist/images/player-sprite-sheet.png', 81, 135, 14);
-    this.game.load.spritesheet('zombie', './dist/images/zombie-sprite-sheet.png', 66, 134, 8);
+    this.game.load.spritesheet('zombie', './dist/images/zombie-sprite-sheet.png', 65.75, 134, 8);
 
   }
 
