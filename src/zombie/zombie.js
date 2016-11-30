35b352442d17b36b37e4d6598efc56fc394e1dd5
Ben Copeland <bscopeland@gmail.com>
2016-11-30 10:07:40 -0500

    Adds randomized zombie behavior

--- src/zombie/zombie.js
+++ src/zombie/zombie.js
@@ -18,17 +18,19 @@ export class Zombie extends Phaser.Sprite {
    * @param  {Number} speed Walking speed for zombie
    * @property {Boolean} alerted Toggles whether the zombie moves toward the player
    */
-  constructor ({ game = {}, health = 100, maxHealth = 100, speed = 10, player = {} } = {}) {
+  constructor ({ game = {}, health = 100, maxHealth = 100, speed = 10, player = {}, perception = 300 } = {}) {
     super(game, spawn.location.x, spawn.location.y, 'zombie');
-    this.player = player;
-    this.config = {
-      scale: 0.4,
-    };
 
     this.health = health;
     this.maxHealth = maxHealth;
     this.speed = speed;
     this.alerted = false;
+    this.perception = perception;
+
+    this.player = player;
+    this.config = {
+      scale: 0.4
+    };
 
     this.render();
   }
@@ -65,23 +67,51 @@ export class Zombie extends Phaser.Sprite {
    * Sets the partol behavior of a zombie
    */
   setPatrol () {
-    if (Math.abs(this.x - this.player.x) < 300) {
+    if (Math.abs(this.x - this.player.x) < this.perception) {
+      this.behaviorDuration = this.game.time.now;
       this.alerted = true;
     } 
     
     if (this.alerted) {
       if (this.x > this.player.x) {
-        this.animations.play('shamble');
-        this.scale.x = -this.config.scale;
-        this.body.velocity.x = -15 * this.speed;
+        this.shamble({ direction: 'left', speedModifier: 30 });
       } else {
-        this.animations.play('shamble');
-        this.scale.x = this.config.scale;
-        this.body.velocity.x = 15 * this.speed;
+        this.shamble({ direction: 'right', speedModifier: 30 });
       }
     } else {
-      this.animations.frame = 0;
+      if (!this.behaviorDuration || this.behaviorDuration <= this.game.time.now) {
+        // Set a duration for a new behavior
+        this.behaviorDuration = this.game.time.now + 5000;
+        // Stand when 0, walk when 1
+        this.behavior = Math.round(Math.random());
+        // Walk left when 0, walk right when 1
+        this.direction = Math.round(Math.random());
+      }
+      if (this.behaviorDuration > this.game.time.now) {
+        if (this.behavior === 1) {
+          if (this.direction === 0) {
+            this.shamble({ direction: 'left', speedModifier: 15 });
+          } else {
+            this.shamble({ direction: 'right', speedModifier: 15 });
+          }
+        } else {
+          this.animations.frame = 0;
+        }
+      }
     }
+  }
+
+  /**
+   * Runs shamble animations
+   * @param {String} direction Direction of animations
+   * @param {Number} speedModifier Multiplier on zombie's default speed attribute
+   */
+  shamble ({ direction = 'left', speedModifier = 15 } = {}) {
+    const velocity = direction === 'left' ? -speedModifier : speedModifier;
+    const scale = direction === 'left' ? -this.config.scale : this.config.scale;
+    this.animations.play('shamble');
+    this.scale.x = scale;
+    this.body.velocity.x = velocity;
   }
 
 }
